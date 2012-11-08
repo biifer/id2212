6f3044899cf0f8ddc3a6e35367e66bfcc7581382
teddycow <fluffgohei@hotmail.com>
2012-11-08 11:00:50 +0100

    port and address update

--- Hangman/src/HangmanClient.java
+++ Hangman/src/HangmanClient.java
@@ -9,15 +9,24 @@ public class HangmanClient {
 	public static void main(String[] args) throws IOException {
 		Socket clientSocket = null;
 		PrintWriter out = null;
-
+		String host;
+		int port;
+		
+		if(args.length > 0)
+			host = args[0];
+		else host = "localhost";
+		if(args.length > 1)
+			port = Integer.parseInt(args[1]);
+		else port = 1234;
+		
 		try {
-			clientSocket = new Socket(args[0], 1234);
+			clientSocket = new Socket(host, port);
 		} catch (UnknownHostException e) {
-			System.err.println("Don't know about host: " + args[0] + ".");
+			System.err.println("Don't know about host: " + host + ".");
 			System.exit(1);
 		} catch (IOException e) {
 			System.err.println("Couldn't get I/O for " +
-					"the connection to: " + args[0] + "");
+					"the connection to: " + host + "");
 			System.exit(1);
 		}
 
